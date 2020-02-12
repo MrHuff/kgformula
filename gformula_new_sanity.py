71ff126e776772292533e91479c040a9eb540cf4
Robert Hu <robert.hu@stats.ox.ac.uk>
2020-02-12 16:57:23 +0000

    fix

--- gformula_new_sanity.py
+++ gformula_new_sanity.py
@@ -13,19 +13,19 @@ if __name__ == '__main__':
         for i in range(seeds):
             X, Y, Z, w = simulate_xyz(n=1000, beta=beta, cor=cor, fam=1, oversamp=2)
             torch.save((X,Y,Z,w),f'./simulated_do_null_fixed/data_seed={i}.pt')
-    # else:
-    #     seeds = 100
-    #     p_value_list = []
-    #     for i in range(seeds):
-    #         X,Y,Z,w = torch.load(f'./simluated_do_null/data_seed={i}.pt')
-    #         #Cheating case
-    #         c = weighted_stat(X=X,Y=Y,Z=Z,get_p_x_cond_z=w)
-    #         reference_metric = c.calculate_weighted_statistic()
-    #         list_of_metrics = []
-    #         for i in range(100):
-    #             list_of_metrics.append(c.permutation_calculate_weighted_statistic())
-    #         array = torch.tensor(list_of_metrics)
-    #         p = calculate_power(array,reference_metric)
-    #         p_value_list.append(p.item())
-    #     plt.hist(p_value_list,bins=seeds//10)
-    #     plt.savefig('./p_value_plot_null=True.png')
+    else:
+        seeds = 100
+        p_value_list = []
+        for i in range(seeds):
+            X,Y,Z,w = torch.load(f'./simulated_do_null_fixed/data_seed={i}.pt')
+            #Cheating case
+            c = weighted_stat(X=X,Y=Y,Z=Z,w=w)
+            reference_metric = c.calculate_weighted_statistic()
+            list_of_metrics = []
+            for i in range(100):
+                list_of_metrics.append(c.permutation_calculate_weighted_statistic())
+            array = torch.tensor(list_of_metrics)
+            p = calculate_power(array,reference_metric)
+            p_value_list.append(p.item())
+        plt.hist(p_value_list,bins=seeds//10)
+        plt.savefig('./simulated_do_null_fixed/p_value_plot_null=True.png')
--- kgformula/test_statistics.py
+++ kgformula/test_statistics.py
@@ -4,19 +4,18 @@ import gpytorch
 from torch.distributions.studentT import StudentT
 import copy
 class weighted_stat():
-    def __init__(self,X,Y,Z,do_null = True,get_p_x_cond_z=None,reg_lambda=1e-3):
+    def __init__(self,X,Y,Z,w,do_null = True,reg_lambda=1e-3):
         self.X = X
         self.Y = Y
         self.Z = Z
         self.n = X.shape[0]
         self.do_null=do_null
-        self.get_p_x_cond_z = get_p_x_cond_z
+        self.W= w@w.t()
         self.kernel_base = gpytorch.kernels.Kernel()
         self.reg_lambda = reg_lambda
         self.H = torch.ones(*(self.n,self.n))*(1-1/self.n)
         for name,data in zip(['kernel_X','kernel_Y','kernel_Z'],[X,Y,Z]):
             self.kernel_ls_init(name,data)
-        self.get_weights()
 
     def kernel_ls_init(self,name,data):
         ker = gpytorch.kernels.RBFKernel()
@@ -29,47 +28,20 @@ class weighted_stat():
             d = self.kernel_base.covar_dist(x1=X,x2=X)
             return torch.sqrt(torch.median(d[d > 0]))
 
-    def cme_x_given_z(self):
-        Z_ker = self.kernel_Z(self.Z)
-        X_ker = self.kernel_X(self.X)
-        with torch.no_grad():
-            mat = gpytorch.add_diag(Z_ker, self.n * self.reg_lambda)
-            mu_embed = gpytorch.inv_matmul(mat,Z_ker,left_tensor=X_ker) #nxn matrix - grid of the mean embedding of X|Z
-            return mu_embed
-
-    def get_weights(self):
-        with torch.no_grad():
-            if self.get_p_x_cond_z is not None:
-                self.w = 1 / self.get_p_x_cond_z
-                self.q_dist = StudentT(df=1)
-                self._q_prob = self.q_dist.log_prob(value=self.X).exp()
-                self.w_adjusted = self._q_prob / self.w
-                self.w_adjusted_sum = self.w_adjusted.sum()
-            else:
-                self.w_adjusted = self.cme_x_given_z()
-                self.w_adjusted_sum = self.w_adjusted.sum()
-            self.w_adjusted_current = copy.deepcopy(self.w_adjusted)
-
-    def calculate_statistic(self):
-        with torch.no_grad():
-            X_ker = self.kernel_X(self.X)
-            Y_ker = self.kernel_Y(self.Y)
-            return (X_ker@self.H@Y_ker).sum()/self.n**2
-
     def permutation_calculate_weighted_statistic(self):
         idx = torch.randperm(self.n)
         with torch.no_grad():
-            X_ker = self.kernel_X(self.X) * self.w_adjusted
-            Y_ker = self.kernel_Y(self.Y)
-            centered = X_ker[idx]@self.H
-            return (centered@Y_ker.evaluate()).sum()/(self.w_adjusted_sum*self.n**2)
+            X_ker = self.kernel_X(self.X)
+            Y_ker = self.kernel_Y(self.Y[idx])
+            A = self.H@gpytorch.matmul(X_ker,self.H)*(self.H@gpytorch.matmul(Y_ker,self.H))
+            return A.sum()
 
     def calculate_weighted_statistic(self):
         with torch.no_grad():
-            X_ker = self.kernel_X(self.X) * self.w_adjusted
+            X_ker = self.kernel_X(self.X)
             Y_ker = self.kernel_Y(self.Y)
-            centered = X_ker@self.H
-            return (centered@Y_ker.evaluate()).sum()/(self.w_adjusted_sum*self.n**2)
+            A = self.H@gpytorch.matmul(X_ker,self.H)*(self.H@gpytorch.matmul(Y_ker,self.H))
+            return A.sum()
 
 
 
